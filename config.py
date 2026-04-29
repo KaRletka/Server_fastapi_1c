79b1fb80da7dfe2e7c7dc9d6d8dada2479335093
KaRletka <muradnazarov2005@gmail.com>
2026-04-29 04:39:47 +0300

    Save

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SECRET_KEY       = "change-me-in-production-use-random-string"
 
 # IP внешнего AI-сервиса (1C AI Bridge). Заполнить вручную, например: "http://192.168.1.50:8000"
-AI_SERVICE_URL   = ""
+AI_SERVICE_URL   = "http://localhost:8888"
 
 # Ref_Key видов цен из Catalog_ВидыЦен
 PRICE_TYPE_RETAIL     = "7481362d-b5b8-11e4-8355-74d02b7dfd8c"  # Розничная
